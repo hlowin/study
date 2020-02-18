62c5a15c404c3314dc4363596d67437ae1de62eb
Hiroaki Yamamoto <hlowinssas@gmail.com>
2020-02-18 23:03:47 +0900

    add files.

--- cuda/ConvertImage/ConvertImage.cu
+++ cuda/ConvertImage/ConvertImage.cu
@@ -8,48 +8,44 @@
 
 __global__
 void GrayscaleKernel(
-	uchar3* img_src,
-	const size_t src_pitch,
-	uchar3* result,
-	const size_t dst_pitch,
+  uchar* img_src,
+  uchar* img_dst,
 	const int width,
 	const int height
 ) {
-	const int x = blockIdx.x * blockDim.x + threadIdx.x;
-	const int y = blockIdx.y * blockDim.y + threadIdx.y;
+  const int i = blockIdx.x * blockDim.x + threadIdx.x;
+  const int x = i % width;
+  const int y = i / width;
 
-	if(x < width && y < height) {
-		int i = x + y * src_pitch / sizeof(uchar3);
-		const uchar3 col = img_src[i];
-		const uchar gs = (col.x + col.y + col.z) / 3;
-		i = x + y * dst_pitch / sizeof(uchar3);
-		result[i] = {gs, gs, gs};
-	}
+  const int img_pos = x * 3 + y * 3 * width;
+  const uchar img_gs = (img_src[img_pos + 0] + img_src[img_pos + 1] + img_src[img_pos + 2]) / 3;
+  img_dst[img_pos + 0] = img_gs;
+  img_dst[img_pos + 1] = img_gs;
+  img_dst[img_pos + 2] = img_gs;
 }
 
 __global__
 void CopyKernel(
-	uchar3* img_src,
-	const size_t src_pitch,
-	uchar3* result,
-	const size_t dst_pitch,
+  uchar* img_src,
+  uchar* img_dst,
 	const int width,
 	const int height
 ) {
-	const int x = blockIdx.x * blockDim.x + threadIdx.x;
-	const int y = blockIdx.y * blockDim.y + threadIdx.y;
+  const int i = blockIdx.x * blockDim.x + threadIdx.x;
+  const int x = i % width;
+  const int y = i / width;
 
-	if(x < width && y < height) {
-		const int i = x + y * src_pitch / sizeof(uchar3);
-		const uchar3 col = img_src[i];
-		result[i] = {col.x, col.y, col.z};
-	}
+  const int img_pos = x * 3 + y * 3 * width;
+  img_dst[img_pos + 0] = img_src[img_pos + 0];
+  img_dst[img_pos + 1] = img_src[img_pos + 1];
+  img_dst[img_pos + 2] = img_src[img_pos + 2];
 }
 
 int main() {
 	rs2::config cfg;
 	const int width = 640;
 	const int height = 480;
+	const int array_size = width * height;
 	cfg.enable_stream(RS2_STREAM_COLOR, width, height, RS2_FORMAT_BGR8, 30);
 
 	rs2::pipeline pipe;
@@ -62,29 +58,23 @@ int main() {
 
 	cv::Mat img_result(size, CV_8UC3);
 
-	uchar3 *img_color_device;
-	size_t color_pitch_device;
-	cudaMallocPitch(&img_color_device, &color_pitch_device, size.width * sizeof(uchar3), size.height);
+	uchar *img_color_device;
+	cudaMalloc(&img_color_device, array_size * 3 * sizeof(uchar));
 
-	uchar3 *img_result_device;
-	size_t result_pitch_device;
-	cudaMallocPitch(&img_result_device, &result_pitch_device, size.width * sizeof(uchar3), size.height);
+	uchar *img_result_device;
+	cudaMalloc(&img_result_device, array_size * 3 * sizeof(uchar));
 
-	// dim3 dimBlock(32, 32);
-	dim3 dimBlock(16, 16);
-	dim3 dimGrid(size.width / dimBlock.x, size.height / dimBlock.y);
+	const int thread = 128;
+	const int block = (size.width * size.height) / thread;
 
 	while (cv::waitKey(1) != 'q') {
 		rs2::frameset frames = pipe.wait_for_frames();
 		rs2::frame color_frame = frames.get_color_frame();
-		std::memcpy(color.data, color_frame.get_data(), width * height * sizeof(uchar3));
+		std::memcpy(color.data, color_frame.get_data(), array_size * 3 * sizeof(uchar));
 
-		cudaMemcpy2D(img_color_device, color_pitch_device, color.data, color.step,
-			size.width * sizeof(uchar3), size.height, cudaMemcpyDefault);
-		CopyKernel<<<dimGrid, dimBlock>>>(img_color_device, color_pitch_device,
-			img_result_device, result_pitch_device, size.width, size.height);
-		cudaMemcpy2D(img_result.data, img_result.step, img_result_device, result_pitch_device,
-			size.width * sizeof(uchar3), size.height, cudaMemcpyDefault);
+		cudaMemcpy(img_color_device, color.data, array_size * 3 * sizeof(uchar), cudaMemcpyHostToDevice);
+		GrayscaleKernel<<<block, thread>>>(img_color_device, img_result_device, size.width, size.height);
+    cudaMemcpy(img_result.data, img_result_device, array_size * 3 * sizeof(uchar), cudaMemcpyDeviceToHost);
 
 		cv::namedWindow("color", cv::WINDOW_AUTOSIZE);
 		cv::imshow("color", color);
---
+++ jetson/documents/REMOTE_MATE.md
@@ -0,0 +1,22 @@
+# Jetson NANOへのリモート接続(ubuntu-mate-desktopを使用)
+
+## 動作環境
+
+- ホストPC
+  - ASUS UX450FDX
+  - Windows10 Home
+    - Tera Term 4.104
+  - Ubuntu 18.04 LTS
+    - Linux Kernel 4.15.0-66-generic
+
+- Jetson NANO
+  - JetPack 4.2.1
+    - Ubuntu 18.04 LTS
+    - Linux Kernal 4.9.140-tegra
+    - CUDA toolkit 10
+  - SANDISK microSD CLASS 10 UHS-1 U3 64GB
+
+## 参考URL
+
+- [リモートデスクトップ環境をUbuntu 18.04 LTSで作成する](https://qiita.com/ryo-endo/items/00f3ec125917acf4cec7)
+- [弱点「ディスプレイマネージャの種類・機能とログイン画面のカスタマイズ」を克服する](https://itjinzai-lab.jp/article/detail/37)
